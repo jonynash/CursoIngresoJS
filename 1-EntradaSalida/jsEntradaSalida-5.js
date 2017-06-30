6020f00366ee647c0e205ae8d8ef951e5d3301b6
Jonatan <jonynash@users.noreply.github.com>
2017-06-30 22:10:24 +0200

    "resuelto" falta confirmar
    venia muy bien jaja

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,15 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+var dato1;
+var dato2;
+dato1 = document.getElementById('elNombre').value;
+dato2 = document.getElementById("laEdad").value;
+
+alert("Usted se llama "+dato1 + " y tiene "+dato2 + " años");
+//Para poder concatenar, se tiene que utlizar el signo suma para unir las variables
+
+
+
 }
 
