a4d755ea6c4567d376c17a49d08e14177af1c34d
Jonatan <jonynash@users.noreply.github.com>
2017-07-02 23:53:09 -0300

    Segundo ejercicio del tp
    me complico mucho entender como hacer la operacion...

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,57 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+var largo;
+var ancho;
+var cantidadAlambre;
+var perimetro;
+largo = document.getElementById('Largo').value;
+ancho = document.getElementById('Ancho').value;
+largo = parseInt(largo);
+ancho = parseInt(ancho);
+
+perimetro = [(largo + ancho)*2] * 3;
+
+
+alert("Debe comprar "+perimetro+ " metros");
+
 
 }
 function Circulo () 
 {
-	
+
+var radio;
+var radioTotal;
+
+radio = document.getElementById('Radio').value;
+
+radioTotal = [3.1415 * (radio*radio)] * 3 ;
+
+alert("Debe comprar "+radioTotal + " metros");
+
 }
+
+
+
 function Materiales () 
 {
+
+var largo;
+var ancho;
+var resultadoCem;
+var resultadoCal;
+
+largo = document.getElementById('Largo').value;
+ancho = document.getElementById('Ancho').value;
+largo = parseInt(largo);
+ancho = parseInt(ancho);
+
+resultadoCem = (largo*ancho)*2;
+resultadoCal = (largo*ancho)*3;
+
+alert("Necesito bolsas "+ resultadoCem + " de cemento"+ " y "+ resultadoCal + " bolsas de cal");
+
+
+
 	
 }
