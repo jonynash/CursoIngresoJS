04553f0133dbc65d586e98887a64e24e58547e33
Jonatan <jonynash@users.noreply.github.com>
2017-07-02 23:47:51 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	//Primer ejercicio que realizo con el enunciado "mostrar en el cuadro de texto" 
+
+	var Ingreseimp; // tomo la variable ingreso que es lo que el cliente va a utilizar
+	var resultadoDes; // variable resultadoDes, esto es la operacion qe el enunciado pide 25% de descuento
+	var resultadoPan; // Esta variable resultadoPan, la creo para que me muestre el resultado en el cuadro de texto pedido
+
+	Ingreseimp = document.getElementById('importe').value;
+	Ingreseimp = parseInt(Ingreseimp);// transformo el valor en un numero entero
+
+	resultadoDes = Ingreseimp * 0.75;// ejecuto la operacion pedida
+
+	resultadoPan = document.getElementById('resultado').value = resultadoDes;
+	// le asigno un valor a la variable resultadoPan tomando el ID del HTML igualandolo al resultado de la operacion
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,12 +4,23 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+//Ejercicio copiado de un compañero, no entendia como hacer que el resultado me aparesca en el recuadro correspondiente...
 
-var sueldo;
+// toma 3 variables
+var importe;
+var resultado1;
 var resultado;
 
+importe= document.getElementById('sueldo').value;// Determina a la variable "importe" con el get.ElementByID Sueldo
+importe= parseInt(importe);//Utiliza el parseInt para convertir el valor en un numero entero
+resultado1= importe*1.1;//Realiza la operacion que pide el ejercicio(mostrar el resultado con un 10% mas)
+resultado1= parseInt(resultado1);// esta linea no me resulta necesaria, dado a que si la elimino el programa ejecuta correctamente porque el importe ya fue modificado con el parseInt
 
-sueldo = document.getElementById('sueldo').value, 
+resultado= document.getElementById('resultado').value=resultado1;
+// para poder plasmar el dato en el cuadro de texto de la pantalla 
+//le doy el valor a la variable en este caso "resultado1" mediante el getElementByID(resultado) 
+//agregandole con el "=" el resultado de la operacion
+//de esta manera al ejecutar el programa lo que me pide el enunciado va a funcionar correctamente
 
 
 }
