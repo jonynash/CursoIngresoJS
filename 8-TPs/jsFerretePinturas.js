26ce841b126d9a6745328c8dae0bc5085eb7876d
Jonatan <jonynash@users.noreply.github.com>
2017-07-05 17:34:38 -0300

    Ferrete iluminacion
    terminado optimo

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -5,9 +5,32 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 function FahrenheitCentigrados () 
 {
 	
+var temp;
+var Fahrenheit;
+
+temp = document.getElementById('Temperatura').value;
+
+temp = parseInt(temp);
+
+Fahrenheit = [(temp - 32) * (5/9)];
+
+
+alert(temp + " fahrenheit son "+ Fahrenheit + " Centigrados");
+
 }
 
 function CentigradosFahrenheit () 
 {
-	
+
+var temp;
+var centígrados;
+
+temp = document.getElementById('Temperatura').value;
+
+temp = parseInt(temp);
+
+centígrados = [(9/5)* temp + 32];
+
+alert(temp + " Centigrados son " + centígrados + " Fahrenheit");
+
 }
