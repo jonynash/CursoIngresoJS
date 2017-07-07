a06fbd577171ae44d22c20823b120b46b0e750db
Jonatan <jonynash@users.noreply.github.com>
2017-07-07 16:43:32 -0300

    ejercicion de if - else
    todos los ejercicios terminados, funcionan todos pero tengo algunas dudas todavia con el implemento de la condicion y el if else

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,18 +1,26 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+//Tomo la variable
 var nota;
+//utlizo la funcion para el numero random dandole un valor a la variable
 nota = Math.floor((Math.random() * 10) + 1);
 
-if(nota =<9)
+//Implemento la condicion mediante un if
+if(nota > 8)
 	{
-		alert("Exelente");
-else if(< 4)
-	alert("aprobo");
-
-
-
+		alert(nota + " Exelente");//Primer alert
+	}	
+else if(nota > 4)//pongo un else if para contrarestar el primer if
+	{
+		alert(nota + " aprobo");//segundo alert
 	}
+	else//utilizo un nuevo else para contradecir la segunda condicion que puse
+		{
+			alert(nota + " Vamos, la proxima se puede");
+		}
+
+//Hay otra manera de escribirlo pero por ahora no lo estoy encontrando
 
 
 }//FIN DE LA FUNCIÓN
