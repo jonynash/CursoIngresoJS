b8bdf737a5b067b0ce0afed545fdcca013ba18ec
Jonatan <jonynash@users.noreply.github.com>
2017-07-12 22:04:25 -0300

    volver a practicarlos
    realizar de nuevo falta entender

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,22 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero = prompt("ingrese un número entre 0 y 10."); 
+
+	while (!(numero >-1 && numero<10))//niengo la condicion de esta manera
+
+	{
+		
+		alert("no sos un genio");//alerta para cuando ingresa algo incorrecto
+		numero = prompt("ingrese un número entre 0 y 10.");//Tomo de nuevo el prompt para que vuelva a pedirme el numero
+
+	}
+
+
+	alert("Bienvenido");//alerta para cuando ingreso lo pedido
+	
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,35 @@
 function Mostrar()
 {
+	//Creo las variables
+	var contador=0;//inicio el contador
+	var acumulador=0;//Inicio el acumulador
+	var numero;
 
-	var contador=0;
-	var acumulador=0;
+	while(contador<5)
+	{
+		contador = contador + 1//Valor literal
+		numero = prompt("Ingrese el numero " + contador + "º:");//Genero la variable numero, y lo tomo por prompt
+		numero = parseInt(numero);
+		acumulador = acumulador + numero;//variable
 
+	}
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+		
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
+
+	/*var salir
+
+	while(salir != "Si") //(true)hace un while "infinito"
+	{
+		console.log("Hola");
+		
+		salir="Si";
+
+								//break;
+		console.log("Chau");
+								//console.log("Chau"); nunca aparece porque el break corta la interacion
+	}
+*/
